b32e64be2f0be38f3fe8555684ad0716e89a8107
Gabriel Cavalheiro <gabriel.cavalheiromr@gmail.com>
2021-09-14 17:33:17 -0300

    Criação da Hero e Modal de acesso

---
+++ front-end/js/main.js
@@ -0,0 +1,25 @@
+//OPEN MODAL
+
+const buttonLogin = document.querySelector('.open-modal')
+const modal = document.getElementById('mymodal');
+const close = document.querySelector('.close');
+
+buttonLogin.addEventListener('click', () => {
+    modal.classList.add('modal-active');
+})
+
+close.addEventListener('click', () => {
+    modal.classList.remove('modal-active');
+})
+
+const buttonHero = document.querySelector('#open-modal')
+const modalHero = document.getElementById('mymodal');
+const closeModal = document.querySelector('.close');
+
+buttonHero.addEventListener('click', () => {
+    modal.classList.add('modal-active');
+})
+
+close.addEventListener('click', () => {
+    modal.classList.remove('modal-active');
+})
