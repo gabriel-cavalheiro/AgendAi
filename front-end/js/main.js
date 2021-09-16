a6889325230600cdb62740f3e471dc159feb1992
Gabriel Cavalheiro <gabriel.cavalheiromr@gmail.com>
2021-09-16 14:50:23 -0300

    refatoração do js

--- front-end/js/main.js
+++ front-end/js/main.js
@@ -1,37 +1,18 @@
 // OPEN/CLOSE MODAL
 
-const buttonLogin = document.querySelector('.open-modal')
-const modal = document.getElementById('mymodal');
-const close = document.querySelector('.close');
+const buttonModal = document.querySelectorAll('.open-modal');
+const modal = document.querySelectorAll('.modal');
+const closed = document.querySelectorAll('.close')
 
-buttonLogin.addEventListener('click', () => {
-    modal.classList.add('modal-active');
-})
+buttonModal.forEach((i) => {
+    i.addEventListener('click', () => {
+      modal[0].classList.add('modal-active');
+    });
+     });
 
-close.addEventListener('click', () => {
-    modal.classList.remove('modal-active');
-})
+closed.forEach((i) => {
+i.addEventListener('click', () => {
+modal[0].classList.remove('modal-active');
+});
+});     
 
-const buttonHero = document.querySelector('#open-modal')
-const modalHero = document.getElementById('mymodal');
-const closeModal = document.querySelector('.close');
-
-buttonHero.addEventListener('click', () => {
-    modalHero.classList.add('modal-active');
-})
-
-close.addEventListener('click', () => {
-    modal.classList.remove('modal-active');
-})
-
-const buttonMeet = document.getElementById('open-modal-m')
-const modalMeet = document.getElementById('mymodal-m');
-const closeModalMeet = document.querySelector('.close');
-
-buttonMeet.addEventListener('click', () => {
-    modal.classList.add('modal-active');
-})
-
-close.addEventListener('click', () => {
-    modal.classList.remove('modal-active');
-})
